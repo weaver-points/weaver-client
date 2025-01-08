18930307131252dd3b97f9b0937ed72b21178f78
chiscookeke11 <chiscookeke11@gmail.com>
2025-01-08 11:27:28 -1100

    fix

--- app/onboard/page.tsx
+++ app/onboard/page.tsx
@@ -5,12 +5,16 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import OnboardScreen1 from "@/components/onboarding-screens/Screen1";
 import OnboardScreen2 from "@/components/onboarding-screens/Screen2";
+import OnboardScreen3 from "@/components/onboarding-screens/Screen3";
+import OnboardScreen4 from "@/components/onboarding-screens/Screen4";
+import OnboardScreen5 from "@/components/onboarding-screens/Screen5";
+import OnboardScreen6 from "@/components/onboarding-screens/Screen6";
 
 export default function Onboard() {
-  const onBoardSlides = [<OnboardScreen1 />, <OnboardScreen2 />];
+  const onBoardSlides = [<OnboardScreen1 />, <OnboardScreen2 />, <OnboardScreen3/>, <OnboardScreen4/>, <OnboardScreen5/>, <OnboardScreen6/> ];
 
   return (
-    <div className="h-screen flex justify-center items-center bg-[#0D0D0D] ">
+    <div className="min-h-screen flex justify-center items-center bg-[#0D0D0D] ">
       <Swiper
         modules={[Autoplay]}
         autoplay={{
@@ -24,7 +28,7 @@ export default function Onboard() {
       >
         {onBoardSlides.map((slide, index) => (
           <SwiperSlide key={index}>
-            <div className="flex justify-center items-center">{slide}</div>
+            <div className=" flex justify-center items-center">{slide}</div>
           </SwiperSlide>
         ))}
       </Swiper>
---
+++ components/onboarding-screens/Screen5.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function OnboardScreen5() {
+  return (
+    <div className="flex flex-col justify-center items-center space-y-5">
+      <p className="text-base" >
+        <span className="font-thin  text-gray-300">Lightweight</span>{" "}
+        <span className="font-bold text-white "> on-chain identity platform</span>
+      </p>
+      <Image
+        src="/first-group-image.svg"
+        alt="group-img-for-screen1"
+        height={128}
+        width={280}
+      />
+      <Image src="/weaver.webp" alt="weaver-logo" height={100} width={100} />
+    </div>
+  );
+}
