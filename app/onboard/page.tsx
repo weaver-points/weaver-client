9a35170cdeaa2eca07c6680361637cf454db3562
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2025-01-09 02:02:06 +0100

    feat: skip btn working

--- app/onboard/page.tsx
+++ app/onboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Autoplay } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import "swiper/css";
 import OnboardScreen1 from "@/components/onboarding-screens/Screen1";
 import OnboardScreen2 from "@/components/onboarding-screens/Screen2";
--- components/register-user/RegisterUser.tsx
+++ components/register-user/RegisterUser.tsx
@@ -3,34 +3,27 @@ import Image from "next/image";
 export default function RegisterUser() {
   return (
     <div className="px-y min-w-[53.75vw] max-w-[1032px] min-h-[63.5vh] max-h-[700px] flex flex-row items-center justify-between px-16  border border-[#414141] rounded-[20px] relative">
-            <small className="absolute top-[10%] right-[10%] text-[#5E5E5E] text-base font-[500] transition duration-300 hover:text-[#EDFFD0] cursor-pointer  " >Skip</small>
-         <Image
-              src="/RegisterUser.svg"
-              alt="group-img-for-screen1"
-              height={100}
-              width={100}
-              className="w-[350px] h-[500] object-contain  self-end "
-            />
-      
-            <div className="min-w-[335px] min-h-[230px]  flex flex-col items-center justify-between text-center text-white  ">
-      
-      
-      
-              <p className="flex flex-col items-center justify-center text-xl lg:text-[28px]   " >
-                <span className="font-light leading-8 " >
-                Mint NFT 
-                </span>
-                <span className="font-bold  leading-8 " >
-                to access
-                <br/>
-                Weaver
+      <Image
+        src="/RegisterUser.svg"
+        alt="group-img-for-screen1"
+        height={100}
+        width={100}
+        className="w-[350px] h-[500] object-contain  self-end "
+      />
 
-                </span>
-              </p>
-              <button className="w-full bg-[#EDFFD0] py-3 text-center text-base text-[#000000] font-[500] rounded-[8px] transform transition duration-300 hover:scale-[1.1] " >Let’s get it🚀</button>
-            </div>
-      
-          </div>
+      <div className="min-w-[335px] min-h-[230px]  flex flex-col items-center justify-between text-center text-white  ">
+        <p className="flex flex-col items-center justify-center text-xl lg:text-[28px]   ">
+          <span className="font-light leading-8 ">Mint NFT</span>
+          <span className="font-bold  leading-8 ">
+            to access
+            <br />
+            Weaver
+          </span>
+        </p>
+        <button className="w-full bg-[#EDFFD0] py-3 text-center text-base text-[#000000] font-[500] rounded-[8px] transform transition duration-300 hover:scale-[1.1] ">
+          Mint
+        </button>
+      </div>
+    </div>
   );
 }
-
