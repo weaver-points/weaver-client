f3814b3304b96a8592b96e340c0b18e40bf083f9
chiscookeke11 <chiscookeke11@gmail.com>
2025-01-09 18:14:10 -1100

    ADD: Task

--- app/onboard/page.tsx
+++ app/onboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Autoplay } from "swiper/modules";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import OnboardScreen1 from "@/components/onboarding-screens/Screen1";
 import OnboardScreen2 from "@/components/onboarding-screens/Screen2";
@@ -33,11 +33,12 @@ export default function Onboard() {
         }}
         spaceBetween={50}
         slidesPerView={1}
-        loop={false}
+        loop={false} 
+        className=" h-screen"
       >
         {onBoardSlides.map((slide, index) => (
           <SwiperSlide key={index}>
-            <div className=" flex justify-center items-center">{slide}</div>
+            <div className=" h-screen w-full flex justify-center items-center">{slide}</div>
           </SwiperSlide>
         ))}
       </Swiper>
--- components/NftContainer.tsx
+++ components/NftContainer.tsx
@@ -1,41 +1,31 @@
-
 import { useState } from "react";
 import TanglerData from "./nftdata/TanglerData";
 import OrbiterData from "./nftdata/OrbiterData";
 
-
 export default function NftContainer() {
-  
+  const [currentNFt, setCurrentNft] = useState(<TanglerData />);
 
-
-
-  const [currentNFt, setCurrentNft] = useState(<TanglerData/>);
-
-  function handleTangler ()  {
-    setCurrentNft(<TanglerData/> )
+  function handleTangler() {
+    setCurrentNft(<TanglerData />);
   }
 
-
-  function handleOrbiter ()  {
-    setCurrentNft(<OrbiterData/> )
+  function handleOrbiter() {
+    setCurrentNft(<OrbiterData />);
   }
-  
-
-
-
-
-
-
-
-
 
   return (
     <div className="w-full ">
       <ul className="flex flex-row items-center gap-6 py-1 pb-2 border-b-2 border-[#1D1D1D] my-2 ">
-        <li onClick={handleTangler} className="px-3 cursor-pointer relative after:content-[' '] after:w-0 after:h-[2px] after:absolute after:bottom-[-10] after:left-0 after:bg-[#E0FFB0] hover:after:w-full after:transition-all after:duration-300 ">
+        <li
+          onClick={handleTangler}
+          className="px-3 cursor-pointer relative after:content-[' '] after:w-0 after:h-[2px] after:absolute after:bottom-[-10] after:left-0 after:bg-[#E0FFB0] hover:after:w-full after:transition-all after:duration-300 "
+        >
           Tangler
         </li>
-        <li onClick={handleOrbiter} className="px-3 cursor-pointer relative after:content-[' '] after:w-0 after:h-[2px] after:absolute after:bottom-[-10] after:left-0 after:bg-[#E0FFB0] hover:after:w-full after:transition-all after:duration-300 ">
+        <li
+          onClick={handleOrbiter}
+          className="px-3 cursor-pointer relative after:content-[' '] after:w-0 after:h-[2px] after:absolute after:bottom-[-10] after:left-0 after:bg-[#E0FFB0] hover:after:w-full after:transition-all after:duration-300 "
+        >
           Orbiter
         </li>
         <li className="px-3 cursor-pointer relative after:content-[' '] after:w-0 after:h-[2px] after:absolute after:bottom-[-10] after:left-0 after:bg-[#E0FFB0] hover:after:w-full after:transition-all after:duration-300 ">
@@ -47,11 +37,6 @@ export default function NftContainer() {
       </ul>
 
       {currentNFt}
-
-
-
-
-    
     </div>
   );
 }
