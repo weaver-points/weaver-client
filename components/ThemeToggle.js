8d4738746b4583a9f8a9d6a2c481b1f239802682
Otaiki1 <otaikisadiq2804@gmail.com>
2025-03-25 20:54:07 +0100

    fix buggy light mode toggle button

--- components/ThemeToggle.js
+++ components/ThemeToggle.js
@@ -4,20 +4,20 @@ import { HiSun } from "react-icons/hi";
 import { useTheme } from "./ThemeContext";
 
 export default function ThemeToggle() {
-  const { theme, toggleTheme } = useTheme();
+    const { theme, toggleTheme } = useTheme();
 
-  return (
-  <div className=" bg-transparent flex justify-end items-end">
-      <button
-      onClick={toggleTheme}
-      className=" p-2 m-2 rounded-full bg-gray-700 hover:bg-gray-600"
-    >
-      <HiSun
-        className={`text-2xl ${
-          theme === "dark" ? "text-yellow-400" : "text-gray-300"
-        }`}
-      />
-    </button>
-  </div>
-  );
+    return (
+        <div className="bg-none flex justify-end items-end">
+            <button
+                onClick={toggleTheme}
+                className=" p-2 m-2 rounded-full bg-gray-700 hover:bg-gray-600"
+            >
+                <HiSun
+                    className={`text-2xl ${
+                        theme === "dark" ? "text-yellow-400" : "text-gray-300"
+                    }`}
+                />
+            </button>
+        </div>
+    );
 }
