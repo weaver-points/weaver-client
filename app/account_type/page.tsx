987ab303e00309b1697ff63978b64d7b9416612c
soomtochukwu <onwuajuesesomtochukwu@gmail.com>
2025-05-31 13:36:30 +0100

    Feat: issue #49  implemented

---
+++ app/account_type/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import AccountTypePage from "@/components/AccountTypePage";
+import React from "react";
+
+const page = () => {
+  const //
+    handleUserClick = () => {},
+    handleProtocolClick = () => {};
+  return (
+    <AccountTypePage
+      onUserClick={handleUserClick}
+      onProtocolClick={handleProtocolClick}
+    ></AccountTypePage>
+  );
+};
+
+export default page;
